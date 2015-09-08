5903764c3498ecef8c0ee9881e3547693e44ed3d
Yang Ming <ming.3.yang@gmail.com>
2015-09-08 10:12:09 +0800

    Untouched LG init version

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 2 14:37:02 KST 2014"
+#define LINUX_COMPILE_BY "jh323.jung"
+#define LINUX_COMPILE_HOST "LGEARND8B8"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
